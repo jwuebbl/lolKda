a48c31c04ddc49ad57247d728ab24c61a69d438e
Your Name <you@example.com>
2023-05-06 12:10:30 -0500

    Deleted vestigal test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { KdaInputFormComponent } from './kda-input-form/kda-input-form.component';
-import { TestFormComponent } from './test-form/test-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    KdaInputFormComponent,
-    TestFormComponent
+    KdaInputFormComponent
   ],
   imports: [
     BrowserModule,
