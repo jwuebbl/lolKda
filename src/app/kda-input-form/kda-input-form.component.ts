74879e13d3fe3b6a254b7669e96a12907b667c3d
Your Name <you@example.com>
2023-05-09 12:00:10 -0500

    Added winlosses to the form

--- src/app/kda-input-form/kda-input-form.component.ts
+++ src/app/kda-input-form/kda-input-form.component.ts
@@ -15,6 +15,7 @@ export class KdaInputFormComponent {
   assists: number = 0;
   numOfAttempts: number = 0;
   selectedCharacter: String = "";
+  winLoss = "";
   leageCharacters: String[] = ["Aatrox", "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir", "Bard", "Bel'Veth", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Draven", "Dr. Mundo", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "K'Sante", "Kai'Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Milio", "Miss Fortune", "Mordekaiser", "Morgana", "Naafiri", "Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu & Willump", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Rell", "Renata Glasc", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vex", "Vi", "Viego", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra"];
 
   ngOnInit() {
@@ -34,18 +35,22 @@ export class KdaInputFormComponent {
     this.deaths = 0;
     this.assists = 0;
     this.selectedCharacter = "";
+    this.winLoss = "";
     (<HTMLInputElement>document.getElementById("kills")).style.backgroundColor = "White";
     (<HTMLInputElement>document.getElementById("deaths")).style.backgroundColor = "White";
     (<HTMLInputElement>document.getElementById("assists")).style.backgroundColor = "White";
     (<HTMLInputElement>document.getElementById("char")).style.backgroundColor = "White";
+    (<HTMLInputElement>document.getElementById("winLoss")).style.backgroundColor = "White";
     (<HTMLInputElement>document.getElementById("statusMessage")).innerText = "";
   }
 
   kdaSubmit() {
     if (this.selectedCharacter == "") {
       (<HTMLInputElement>document.getElementById("char")).style.backgroundColor = "Red";
+    } else if (this.winLoss == "") {
+      (<HTMLInputElement>document.getElementById("winLoss")).style.backgroundColor = "Red";
     } else if (this.kills < 0) {
-        (<HTMLInputElement>document.getElementById("kills")).style.backgroundColor = "Red";
+      (<HTMLInputElement>document.getElementById("kills")).style.backgroundColor = "Red";
     } else if (this.deaths < 0) {
         (<HTMLInputElement>document.getElementById("deaths")).style.backgroundColor = "Red";
     } else if (this.assists < 0) {
@@ -67,6 +72,7 @@ export class KdaInputFormComponent {
         kills: this.kills,
         deaths: this.deaths,
         assists: this.assists,
+        winLoss: this.winLoss
         };
         this.http.post('/submitLeagueGame', data).subscribe(observer);
     } 
