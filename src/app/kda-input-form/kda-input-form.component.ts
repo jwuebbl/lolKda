ddde8fb1a9ecc056ab4532d2ed3eb429a906d33b
Your Name <you@example.com>
2023-05-08 23:33:56 -0500

    Shows who is signed in on the submigLeagueGame page

--- src/app/kda-input-form/kda-input-form.component.ts
+++ src/app/kda-input-form/kda-input-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
@@ -9,12 +9,25 @@ import { HttpClient } from '@angular/common/http';
 })
 export class KdaInputFormComponent {
   constructor(private http: HttpClient) {}
+  username: String = "";
   kills: number = 0;
   deaths: number = 0;
   assists: number = 0;
   numOfAttempts: number = 0;
   selectedCharacter: String = "";
   leageCharacters: String[] = ["Aatrox", "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir", "Bard", "Bel'Veth", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Draven", "Dr. Mundo", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "K'Sante", "Kai'Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Milio", "Miss Fortune", "Mordekaiser", "Morgana", "Naafiri", "Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu & Willump", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Rell", "Renata Glasc", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vex", "Vi", "Viego", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra"];
+
+  ngOnInit() {
+    const observer = {
+      next: (response: any) => {
+        this.username = response['username'];
+      },
+      error: (error: any) => {
+        console.log('POST request error:', error);
+      }
+    };
+    this.http.get('/username').subscribe(observer)
+  }
 
   resetAllFields() {
     this.kills = 0;
@@ -25,6 +38,7 @@ export class KdaInputFormComponent {
     (<HTMLInputElement>document.getElementById("deaths")).style.backgroundColor = "White";
     (<HTMLInputElement>document.getElementById("assists")).style.backgroundColor = "White";
     (<HTMLInputElement>document.getElementById("char")).style.backgroundColor = "White";
+    (<HTMLInputElement>document.getElementById("statusMessage")).innerText = "";
   }
 
   kdaSubmit() {
