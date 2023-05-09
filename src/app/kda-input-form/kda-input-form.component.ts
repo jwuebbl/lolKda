98a6848fdf23255f027e5361d4f1b55c47e47caf
Your Name <you@example.com>
2023-05-09 12:58:04 -0500

    Post message is now shwon and removed correctly

--- src/app/kda-input-form/kda-input-form.component.ts
+++ src/app/kda-input-form/kda-input-form.component.ts
@@ -14,8 +14,9 @@ export class KdaInputFormComponent {
   deaths: number = 0;
   assists: number = 0;
   numOfAttempts: number = 0;
+  numOfSuccessfulPosts: number = 0;
   selectedCharacter: String = "";
-  winLoss = "";
+  winLoss: String = "";
   leageCharacters: String[] = ["Aatrox", "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir", "Bard", "Bel'Veth", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Draven", "Dr. Mundo", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "K'Sante", "Kai'Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Milio", "Miss Fortune", "Mordekaiser", "Morgana", "Naafiri", "Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu & Willump", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Rell", "Renata Glasc", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vex", "Vi", "Viego", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra"];
 
   ngOnInit() {
@@ -30,7 +31,7 @@ export class KdaInputFormComponent {
     this.http.get('/username').subscribe(observer)
   }
 
-  resetAllFields() {
+  resetAllFields(clicked:boolean) {
     this.kills = 0;
     this.deaths = 0;
     this.assists = 0;
@@ -41,7 +42,9 @@ export class KdaInputFormComponent {
     (<HTMLInputElement>document.getElementById("assists")).style.backgroundColor = "White";
     (<HTMLInputElement>document.getElementById("char")).style.backgroundColor = "White";
     (<HTMLInputElement>document.getElementById("winLoss")).style.backgroundColor = "White";
-    (<HTMLInputElement>document.getElementById("statusMessage")).innerText = "";
+    if (clicked == true) {
+      (<HTMLInputElement>document.getElementById("statusMessage")).innerText = "";
+    }
   }
 
   kdaSubmit() {
@@ -58,8 +61,10 @@ export class KdaInputFormComponent {
     } else {
         const observer = {
           next: (response: any) => {
-            (<HTMLInputElement>document.getElementById("statusMessage")).innerText = "Data saved successfully! I'm ready for another request.";
-            this.resetAllFields();
+            this.numOfSuccessfulPosts += 1;
+            (<HTMLInputElement>document.getElementById("statusMessage")).innerText = "Data saved successfully! Number of successful POSTs: " + this.numOfSuccessfulPosts;
+            let clicked = false;
+            this.resetAllFields(clicked);
           },
           error: (error: any) => {
             console.log('POST request error:', error);
