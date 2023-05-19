0f62be447d7e0b3e90d5a62d92a89a855b9b4315
Your Name <you@example.com>
2023-05-18 23:51:51 -0500

    Fixed rename issue in script

--- adjustFileNames.sh
+++ adjustFileNames.sh
@@ -1,9 +1,9 @@
-echo "Getting the built file names."
+echo "Finding the built file names."
 runtimeJs=$(ls dist/lol-kda/ | grep runtime)
 polyfillsJs=$(ls dist/lol-kda/ | grep polyfills)
 mainJs=$(ls dist/lol-kda/ | grep main)
 stylesCss=$(ls dist/lol-kda/ | grep styles)
-echo "File names collected: $runtimeJs, $polyfillsJs, $mainJs, $stylesCss"
+echo "File names found: $runtimeJs, $polyfillsJs, $mainJs, $stylesCss"
 
 echo "Renaming $runtimeJs to lolKdaRunTime.js"
 mv dist/lol-kda/$runtimeJs dist/lol-kda/lolKdaRunTime.js
@@ -15,4 +15,4 @@ echo "Renaming $mainJs to lolKdaMain.js"
 mv dist/lol-kda/$mainJs dist/lol-kda/lolKdaMain.js
 
 echo "Renaming $stylesCss to lolKdaStyles.js"
-mv dist/lol-kda/$stylesCss dist/lol-kda/lolKdaStyles.js
+mv dist/lol-kda/$stylesCss dist/lol-kda/lolKdaStyles.css
