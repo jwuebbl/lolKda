7b8a3f8507991e671fe02b6e9fd468cf95a799ea
Your Name <you@example.com>
2023-05-18 23:07:58 -0500

    Fixing errors that occured during the pipeline

--- adjustFileNames.sh
+++ adjustFileNames.sh
@@ -6,13 +6,13 @@ stylesCss=$(ls dist/lol-kda/ | grep styles)
 echo "File names collected: $runtimeJs, $polyfillsJs, $mainJs, $stylesCss"
 
 echo "Renaming $runtimeJs to lolKdaRunTime.js"
-mv ./$runtimeJs ./lolKdaRunTime.js
+mv dist/lol-kda/$runtimeJs dist/lol-kda/lolKdaRunTime.js
 
 echo "Renaming $polyfillsJs to lolKdaPolyfills.js"
-mv ./$polyfillsJs ./lolKdaPolyfills.js
+mv dist/lol-kda/$polyfillsJs dist/lol-kda/lolKdaPolyfills.js
 
 echo "Renaming $mainJs to lolKdaMain.js"
-mv ./$mainJs ./lolKdaMain.js
+mv dist/lol-kda/$mainJs dist/lol-kda/lolKdaMain.js
 
 echo "Renaming $stylesCss to lolKdaStyles.js"
-mv ./$stylesCss ./lolKdaStyles.js
+mv dist/lol-kda/$stylesCss dist/lol-kda/lolKdaStyles.js
